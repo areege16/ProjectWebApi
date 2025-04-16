55a542493a10ea0513ba1c50cd60917643d9be06
Mohamed Ehab <mohamedehaab14@gmail.com>
2025-04-16 22:47:58 +0200

    user relation

--- WebApiProject/Models/ApplicationUser.cs
+++ WebApiProject/Models/ApplicationUser.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebApiProject.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [InverseProperty("Sender")]
+        public ICollection<ChatFoundItem>? SentMsgs { get; set; }
 
+
+
+        [InverseProperty("Receiver")]
+        public ICollection<ChatFoundItem>? ReceivedMsgs { get; set; }
     }
 }

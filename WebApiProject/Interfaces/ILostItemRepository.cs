fa62dba879fc05273a9d322c0ca35b347fdaeea4
areege16 <areege162002@gmail.com>
2025-04-18 16:19:18 +0200

    aaa

---
+++ WebApiProject/Interfaces/ILostItemRepository.cs
@@ -0,0 +1,8 @@
+﻿using WebApiProject.Models;
+
+namespace WebApiProject.Interfaces
+{
+    public interface ILostItemRepository:IGenericRepository<ItemLost>
+    {
+    }
+}
--- WebApiProject/Program.cs
+++ WebApiProject/Program.cs
@@ -1,3 +1,148 @@
+﻿
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Infrastructure;
+//using Microsoft.IdentityModel.Tokens;
+//using System.Text;
+//using WebApiProject.Helpers;
+//using WebApiProject.Hubs;
+//using WebApiProject.Interfaces;
+//using WebApiProject.Models;
+//using WebApiProject.Service;
+//using Microsoft.AspNetCore.Authentication.Google;
+//using Microsoft.AspNetCore.Authentication.Cookies;
+
+
+//namespace WebApiProject
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var builder = WebApplication.CreateBuilder(args);
+
+//            // Add services to the container.
+
+//            builder.Services.AddControllers();
+
+//            builder.Services.AddHttpContextAccessor();
+
+
+//            //connectionString
+//            builder.Services.AddDbContext<LostFoundContext>(option =>
+//            {
+//                option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//            });
+
+//          //  for authentication
+//            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//                .AddEntityFrameworkStores<LostFoundContext>();
+
+//            //setting authentication middleware check using JWT Token
+//            builder.Services.AddAuthentication(options =>
+//            {
+//                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+//                //options.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+
+//            })
+//                //.AddGoogle(options =>
+//                //{
+//                //    options.ClientId = builder.Configuration["Google:ClientId"]; 
+//                //    options.ClientSecret = builder.Configuration["Google:ClientSecret"]; 
+//                //})
+
+//                .AddJwtBearer(options =>
+//                {
+//                    options.SaveToken = true;
+//                    options.RequireHttpsMetadata = false;
+//                    options.TokenValidationParameters = new()
+//                    {
+//                        ValidateIssuer = true,
+//                        ValidIssuer = builder.Configuration["JWT:Iss"],
+//                        ValidateAudience = true,
+//                        ValidAudience = builder.Configuration["JWT:Aud"],
+//                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+//                    };
+//                });
+
+
+//            //// Add Identity
+//            //builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//            //    .AddEntityFrameworkStores<LostFoundContext>()
+//            //    .AddDefaultTokenProviders();
+
+
+//            //builder.Services.AddAuthentication(options =>
+//            //{
+//            //    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme; 
+//            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//            //})
+//            //.AddCookie() 
+//            //.AddJwtBearer(options =>
+//            //{
+//            //    options.SaveToken = true;
+//            //    options.RequireHttpsMetadata = false;
+//            //    options.TokenValidationParameters = new TokenValidationParameters
+//            //    {
+//            //        ValidateIssuer = true,
+//            //        ValidIssuer = builder.Configuration["JWT:Iss"],
+//            //        ValidateAudience = true,
+//            //        ValidAudience = builder.Configuration["JWT:Aud"],
+//            //        IssuerSigningKey = new SymmetricSecurityKey(
+//            //            Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+//            //    };
+//            //})
+//            //.AddGoogle(options =>
+//            //{
+//            //    options.ClientId = builder.Configuration["Google:ClientId"];
+//            //    options.ClientSecret = builder.Configuration["Google:ClientSecret"];
+//            //    options.CallbackPath = "/signin-google"; 
+//            //});
+
+
+
+
+
+//            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//            builder.Services.AddEndpointsApiExplorer();
+//            builder.Services.AddSwaggerGen();
+
+//            //chatFound
+//            builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+//            builder.Services.AddScoped<IChatFoundService, ChatFoundService>();
+//            builder.Services.AddSignalR();
+
+//            var app = builder.Build();
+
+//            // Configure the HTTP request pipeline.
+//            if (app.Environment.IsDevelopment())
+//            {
+//                app.UseSwagger();
+//                app.UseSwaggerUI();
+//            }
+
+//            app.UseRouting();
+
+//            app.UseCookiePolicy();
+
+//            builder.Services.AddSession();
+//            app.UseSession();
+
+//            app.UseAuthentication();
+//            app.UseAuthorization();
+
+//            app.MapHub<ChatFoundHub>("/chatFoundHub");
+//            app.MapControllers();
+
+//            app.Run();
+//        }
+//    }
+//}
+
 
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -9,6 +154,7 @@ using WebApiProject.Helpers;
 using WebApiProject.Hubs;
 using WebApiProject.Interfaces;
 using WebApiProject.Models;
+using WebApiProject.Repository;
 using WebApiProject.Service;
 
 namespace WebApiProject
@@ -28,6 +174,9 @@ namespace WebApiProject
             {
                 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            builder.Services.AddScoped<IFoundItemRepository, FoundItemRepository>();
+
 
             //for authentication
             builder.Services.AddIdentity<ApplicationUser, IdentityRole>()

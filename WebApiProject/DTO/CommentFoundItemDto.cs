d47c17a3e93739a32ae37a3a1e0603188cfd013e
areege16 <areege162002@gmail.com>
2025-04-19 13:51:37 +0200

    dddd

---
+++ WebApiProject/DTO/CommentFoundItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiProject.DTO
+{
+    public class CommentFoundItemDto
+    {
+        //public int ID { get; set; }
+        public string Description { set; get; }
+        public DateTime CreatedAt { get; set; }
+        public string UserName { set; get; }
+        public int ItemFoundID { set; get; }
+
+    }
+}

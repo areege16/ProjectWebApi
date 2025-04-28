a9c030e707b1b0b75e0ea215ed36d218973603e4
areege16 <areege162002@gmail.com>
2025-04-28 17:14:39 +0300

    AddFoundComment

---
+++ WebApiProject/DTO/AddLostItemCommentDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiProject.DTO
+{
+    public class AddLostItemCommentDTO
+    {
+        public string Description { set; get; }
+        public DateTime CreatedAt { get; set; }
+        public string UserId { set; get; }
+        public int ItemLostID { set; get; }
+
+    }
+}
--- WebApiProject/Program.cs
+++ WebApiProject/Program.cs
@@ -144,6 +144,7 @@
 //}
 
 
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -178,6 +179,8 @@ namespace WebApiProject
             });
 
             builder.Services.AddScoped<IFoundItemRepository, FoundItemRepository>();
+            builder.Services.AddScoped<IFoundCommentRepo, FoundCommentRepo>();
+
 
 
             //for authentication
